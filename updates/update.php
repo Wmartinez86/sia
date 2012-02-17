1201ed3ddeddeb42d85d68b40afece46121b8764
brau <soncco@gmail.com>
2012-02-16 19:20:05 -0500

    Actualizacion 001

--- updates/update.php
+++ updates/update.php
@@ -8,27 +8,37 @@ include("../redirect.php");
 function update_001() {
     global $bcdb;
     
-    $sql = "
-        ALTER TABLE  `o_detallecotizacion` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
+    $bcdb->show_errors = true;
 
-        ALTER TABLE  `o_detallenea` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-        ALTER TABLE  `o_detallenea` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-
-        ALTER TABLE  `o_detalleordencompra` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-        ALTER TABLE  `o_detalleordencompra` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-
-        ALTER TABLE  `o_detalleordenservicio` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-        ALTER TABLE  `o_detalleordenservicio` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-
-        ALTER TABLE  `o_detallepecosa` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-        ALTER TABLE  `o_detallepecosa` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-
-        ALTER TABLE  `o_detallerequerimiento` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';
-        ";
+    $sql = "ALTER TABLE  `o_detallecotizacion` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
     
-    return $bcdb->query($sql);
+    $sql = "ALTER TABLE  `o_detallenea` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    $sql = "ALTER TABLE  `o_detallenea` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    
+    $sql = "ALTER TABLE  `o_detalleordencompra` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    $sql = "ALTER TABLE  `o_detalleordencompra` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    
+    $sql = "ALTER TABLE  `o_detalleordenservicio` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    $sql = "ALTER TABLE  `o_detalleordenservicio` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    
+    $sql = "ALTER TABLE  `o_detallepecosa` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    $sql = "ALTER TABLE  `o_detallepecosa` CHANGE  `precio`  `precio` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    
+    $sql = "ALTER TABLE  `o_detallerequerimiento` CHANGE  `cantidad`  `cantidad` DECIMAL( 10, 2 ) NOT NULL DEFAULT  '0';";
+    $bcdb->query($sql);
+    
+    $bcdb->show_errors = false;
 }
 
-print (update_001()) ? "Actualizacion 001 completada" : "No se pudo completar la actualizacion 001";
+update_001();
 
 ?>
