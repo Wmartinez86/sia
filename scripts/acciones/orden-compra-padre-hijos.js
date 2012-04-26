6872c34752aa9ae30d8055ef0132443d184d25d2
brau <soncco@gmail.com>
2012-04-26 06:06:10 -0500

    Mas de PAdres

---
+++ scripts/acciones/orden-compra-padre-hijos.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#enlazar').click(function () {
+        var op = $("input[name='op']:checked").val();
+        var idpadre = $("#idpadre").val();
+        var url = 'orden-compra-hijo.php?idpadre=' + idpadre + '&op=' + op;
+
+        var params = 'location=0,status=0,scrollbars=0,width=850,height=550';
+        window.open(url, 'orden-compra-hijo', params);
+    });
+});
