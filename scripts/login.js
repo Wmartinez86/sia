c4dc9400c5f0e2f2ee90483e2bbea3935861158e
brau <soncco@gmail.com>
2013-01-09 00:51:15 -0500

    Login 2013

---
+++ scripts/login.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('#frmlogin').submit(function() {
+		if ($('#anio').val() == '2012' ) {
+			var locURL = window.location.href;
+			var sub = locURL.split('.');
+      var sub = sub[0]
+			$(this).attr('action', sub.substr(0, sub.length-1) + '.abastecimiento.pe/login.php');
+		}
+	});
+});
